36a67b8ca500ce97b5c460a8994f50c6494c0885
Chris Nesbitt-Smith <chris@cns.me.uk>
2017-08-11 13:03:35 +0100

    change to allow https

--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const _ = require('lodash')
 const app = express()
 app.use(cors())
 
-const {SYNOLOGY_IP, SYNOLOGY_PORT, SYNOLOGY_USER, SYNOLOGY_PASS} = process.env;
+const {SYNOLOGY_HOST, SYNOLOGY_USER, SYNOLOGY_PASS} = process.env;
 
 var session_id
 var cams
 
-const authenticate = () => request(`http://${SYNOLOGY_IP}:${SYNOLOGY_PORT}/webapi/auth.cgi?api=SYNO.API.Auth&method=Login&version=3&account=${SYNOLOGY_USER}&passwd=${SYNOLOGY_PASS}&session=SurveillanceStation&format=sid`)
+const authenticate = () => request(`${SYNOLOGY_HOST}/webapi/auth.cgi?api=SYNO.API.Auth&method=Login&version=3&account=${SYNOLOGY_USER}&passwd=${SYNOLOGY_PASS}&session=SurveillanceStation&format=sid`)
   .then(JSON.parse)
   .tap(console.info)
   .then(response => session_id = response.data.sid)
 
-const get_cameras = () => request(`http://${SYNOLOGY_IP}:${SYNOLOGY_PORT}/webapi/entry.cgi?api=SYNO.SurveillanceStation.Camera&method=List&version=3&_sid=${session_id}`)
+const get_cameras = () => request(`${SYNOLOGY_HOST}/webapi/entry.cgi?api=SYNO.SurveillanceStation.Camera&method=List&version=3&_sid=${session_id}`)
   .then(JSON.parse)
   .tap(console.info)
   .then(response => cameras = response.data.cameras)
 
-const get_feed_urls = () => _.map(cameras, camera => `http://${SYNOLOGY_IP}:${SYNOLOGY_PORT}/webapi/SurveillanceStation/videoStreaming.cgi?api=SYNO.SurveillanceStation.VideoStream&version=1&method=Stream&cameraId=${camera.id}&format=mjpeg&_sid=${session_id}`)
+const get_feed_urls = () => _.map(cameras, camera => `${SYNOLOGY_HOST}/webapi/SurveillanceStation/videoStreaming.cgi?api=SYNO.SurveillanceStation.VideoStream&version=1&method=Stream&cameraId=${camera.id}&format=mjpeg&_sid=${session_id}`)
 
 authenticate()
   .then(get_cameras)
